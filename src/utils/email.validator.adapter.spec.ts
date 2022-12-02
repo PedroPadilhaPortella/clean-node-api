471d38882aa1fc3b04a4174b707c4b7afaeba9cf
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-01 23:43:23 -0300

    feat: ensure EmailValidatorAdapter return a boolean if the email is valid or not

--- src/utils/email.validator.adapter.spec.ts
+++ src/utils/email.validator.adapter.spec.ts
@@ -1,9 +1,24 @@
 import { EmailValidatorAdapter } from './email.validator.adapter'
+import validator from 'validator'
+
+jest.mock('validator', () => ({
+  isEmail (): boolean {
+    return true
+  }
+}))
 
 describe('EmailValidatorAdapter', () => {
+
   it('should return false if email is not valid', () => {
+    jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const sut = new EmailValidatorAdapter()
     const isValid = sut.isValid('invalid_email@mail.com')
     expect(isValid).toBe(false)
+  })
+  
+  it('should return true if email is valid', () => {
+    const sut = new EmailValidatorAdapter()
+    const isValid = sut.isValid('valid.email@mail.com')
+    expect(isValid).toBe(true)
   })
 })

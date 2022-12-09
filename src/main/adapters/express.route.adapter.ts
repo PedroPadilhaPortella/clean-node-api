7c07c5f3fe693bfc5166647b1cff6c271d85fcd5
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-09 02:08:05 -0300

    feat: ensure expressRouteAdapter returns error message

--- src/main/adapters/express.route.adapter.ts
+++ src/main/adapters/express.route.adapter.ts
@@ -5,6 +5,11 @@ export const adapteRoute = (controller: Controller) => {
   return async (req: Request, res: Response) => {
     const httpRequest: HttpRequest = { body: req.body }
     const httpResponse = await controller.handle(httpRequest)
+
+    if (httpResponse.statusCode === 500) {
+      res.status(httpResponse.statusCode).json({ error: httpResponse.body.message })
+    }
+
     res.status(httpResponse.statusCode).json(httpResponse.body)
   }
 }

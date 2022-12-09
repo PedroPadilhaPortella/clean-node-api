78ece69d46cfb89bdf81a161590e884ca2bed814
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-09 01:33:24 -0300

    feat: implements LogMongoRepository as a LogErrorRepository for SignUpControllerFactory

--- src/main/decorators/log.spec.ts
+++ src/main/decorators/log.spec.ts
@@ -1,10 +1,7 @@
+import { LogErrorRepository } from "../../data/protocols/log-error-repository.interface"
+import { Ok, ServerError } from "../../presentation/helpers/http.helper"
 import { Controller, HttpRequest, HttpResponse } from "../../presentation/protocols"
 import { LogControllerDecorator } from "./log"
-
-interface SutTypes {
-  sut: LogControllerDecorator
-  controllerStub: Controller
-}
 
 const makeController = (): Controller => {
   class ControllerStub implements Controller {
@@ -16,27 +13,61 @@ const makeController = (): Controller => {
   return new ControllerStub()
 }
 
+const makeLogErrorRepository = (): LogErrorRepository => {
+  class LogErrorRepositoryStub implements LogErrorRepository {
+    async logError (stackError: string): Promise<void> {
+      return await new Promise(resolve => resolve())
+    }
+  }
+  return new LogErrorRepositoryStub()
+}
+
+interface SutTypes {
+  sut: LogControllerDecorator
+  controllerStub: Controller
+  logErrorRepositoryStub: LogErrorRepository
+}
+
 const makeSut = (): SutTypes => {
   const controllerStub = makeController()
-  const sut = new LogControllerDecorator(controllerStub)
-  return { sut, controllerStub }
+  const logErrorRepositoryStub = makeLogErrorRepository()
+  const sut = new LogControllerDecorator(controllerStub, logErrorRepositoryStub)
+  return { sut, controllerStub, logErrorRepositoryStub }
 }
+
+const makeFakeRequest = (): HttpRequest => ({
+  body: {
+    name: 'pedro',
+    email: 'email@gmail.com',
+    password: 'pedro123',
+    passwordConfirmation: 'pedro123'
+  }
+})
 
 describe('LogControllerDecorator', () => {
 
   it('should call controller handle', async () => {
     const { sut, controllerStub } = makeSut()
     const handleSpy = jest.spyOn(controllerStub, 'handle')
-    const httpRequest: HttpRequest = {
-      body: {
-        name: 'pedro',
-        email: 'mail@gmail.com',
-        password: 'pedro123',
-        passwordConfirmation: 'pedro123'
-      }
-    }
-    const response = await sut.handle(httpRequest)
+    const httpRequest = makeFakeRequest()
+    const httpResponse = await sut.handle(httpRequest)
     expect(handleSpy).toHaveBeenCalledWith(httpRequest)
-    expect(response).toEqual({ body: {}, statusCode: 200 })
+    expect(httpResponse).toEqual(Ok({}))
+  })
+
+  it('should call LogErrorRepository when controller returns a InternalServerError', async () => {
+    const { sut, controllerStub, logErrorRepositoryStub } = makeSut()
+
+    const fakeError = new Error()
+    fakeError.message = 'Error Stack'
+
+    const logSpy = jest.spyOn(logErrorRepositoryStub, 'logError')
+    jest.spyOn(controllerStub, 'handle')
+      .mockReturnValueOnce(new Promise(resolve => resolve(ServerError(fakeError))))
+
+    const httpRequest = makeFakeRequest()
+    const response = await sut.handle(httpRequest)
+    expect(logSpy).toHaveBeenCalledWith('Error Stack')
+    expect(response).toEqual(ServerError(new Error('Error Stack')))
   })
 })
--- src/main/factories/signup.ts
+++ src/main/factories/signup.ts
@@ -1,6 +1,7 @@
 import { DbAddAccount } from '../../data/usecases/add-account/db-add-account'
 import { BCryptAdapter } from '../../infra/criptography/bcrypt.adapter'
 import { AccountMongoRepository } from '../../infra/db/mongodb/account-repository/account'
+import { LogMongoRepository } from '../../infra/db/mongodb/log-repository/log'
 import { SignUpController } from '../../presentation/controllers/signup/signup'
 import { Controller } from '../../presentation/protocols'
 import { EmailValidatorAdapter } from '../../utils/email.validator.adapter'
@@ -10,8 +11,9 @@ export const SignUpControllerFactory = (): Controller => {
   const salt = 12
   const emailValidator = new EmailValidatorAdapter()
   const bCryptAdapter = new BCryptAdapter(salt)
+  const logMongoRepository = new LogMongoRepository()
   const accountMongoRepository = new AccountMongoRepository()
   const dbAddAccount = new DbAddAccount(accountMongoRepository, bCryptAdapter)
   const signUpController = new SignUpController(dbAddAccount, emailValidator)
-  return new LogControllerDecorator(signUpController)
+  return new LogControllerDecorator(signUpController, logMongoRepository)
 }

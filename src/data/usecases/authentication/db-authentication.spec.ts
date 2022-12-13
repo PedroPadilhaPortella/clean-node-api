5dd83a406d1cb576ebeec77fc08bca3c6d265ea6
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-13 18:14:38 -0300

    refactor: move duplicated code to a factory

--- src/data/usecases/authentication/db-authentication.spec.ts
+++ src/data/usecases/authentication/db-authentication.spec.ts
@@ -1,21 +1,36 @@
+import { Authentication } from '../../../domain/usecases/authentication.interface'
 import { LoadAccountByEmailRepository } from '../../protocols/load-account-repository.interface'
 import { AccountModel } from './../../../domain/models/account.model'
 import { DbAuthentication } from './db-authentication'
 
-class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+const autentication = { email: 'email@mail.com', password: 'pass123' }
 
-  async load (email: string): Promise<AccountModel> {
-    return { id: '1', email, name: 'name', password: 'pass123' }
+const createLoadAccount = (): LoadAccountByEmailRepository => {
+  class LoadAccountByEmailRepositoryStub implements LoadAccountByEmailRepository {
+    async load (email: string): Promise<AccountModel> {
+      return { id: '1', email, name: 'name', password: 'pass123' }
+    }
   }
+  return new LoadAccountByEmailRepositoryStub()
+}
+
+interface SutTypes {
+  loadAccountByEmailRepositoryStub: LoadAccountByEmailRepository
+  sut: Authentication
+}
+
+const makeSut = (): SutTypes => {
+  const loadAccountByEmailRepositoryStub = createLoadAccount()
+  const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+  return { sut, loadAccountByEmailRepositoryStub }
 }
 
 describe('DbAuthentication', () => { 
   
   it('should call LoadAccountByEmailRepository with correct email', async () => {
-    const loadAccountByEmailRepositoryStub = new LoadAccountByEmailRepositoryStub()
-    const sut = new DbAuthentication(loadAccountByEmailRepositoryStub)
+    const { sut, loadAccountByEmailRepositoryStub } = makeSut()
     const loadSpy = jest.spyOn(loadAccountByEmailRepositoryStub, 'load')
-    await sut.authenticate({ email: 'email@mail.com', password: 'pass123' })
-    expect(loadSpy).toBeCalledWith('email@mail.com')
+    await sut.authenticate(autentication)
+    expect(loadSpy).toBeCalledWith(autentication.email)
   })
 })

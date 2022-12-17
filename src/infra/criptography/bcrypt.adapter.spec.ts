d474e9b6c6a356ba5951539975485da6a0d34f4e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-17 20:05:10 -0300

    feat: ensure BcryptAdapter calls compare with correct values and return a boolean on success

--- src/infra/criptography/bcrypt.adapter.spec.ts
+++ src/infra/criptography/bcrypt.adapter.spec.ts
@@ -8,6 +8,9 @@ const hashedPassword = 'hashed_pass'
 jest.mock('bcrypt', () => ({
   async hash (): Promise<string> {
     return await new Promise(resolve => resolve(hashedPassword))
+  },
+  async compare (): Promise<boolean> {
+    return await new Promise(resolve => resolve(true))
   }
 }))
 
@@ -17,16 +20,33 @@ const makeSut = (): BCryptAdapter => {
 
 describe('BCryptAdapter', () => {
 
-  it('should call bCrypt with correct values', async () => {
-    const sut = makeSut()
-    const hashSpy = jest.spyOn(bcrypt, 'hash')
-    await sut.hash(password)
-    expect(hashSpy).toHaveBeenCalledWith(password, salt)
+  describe('hash method', () => {
+    it('should call hash with correct values', async () => {
+      const sut = makeSut()
+      const hashSpy = jest.spyOn(bcrypt, 'hash')
+      await sut.hash(password)
+      expect(hashSpy).toHaveBeenCalledWith(password, salt)
+    })
+    
+    it('should return a hash on success', async () => {
+      const sut = makeSut()
+      const hash = await sut.hash(password)
+      expect(hash).toBe(hashedPassword)
+    })
   })
-  
-  it('should return a hash on success', async () => {
-    const sut = makeSut()
-    const hash = await sut.hash(password)
-    expect(hash).toBe(hashedPassword)
+
+  describe('compare method', () => {
+    it('should call compare with correct values', async () => {
+      const sut = makeSut()
+      const compareSpy = jest.spyOn(bcrypt, 'compare')
+      await sut.compare(password, hashedPassword)
+      expect(compareSpy).toHaveBeenCalledWith(password, hashedPassword)
+    })
+    
+    it('should return a boolean on success', async () => {
+      const sut = makeSut()
+      const compare = await sut.compare(password, hashedPassword)
+      expect(compare).toBeTruthy()
+    })
   })
 })
--- src/infra/criptography/bcrypt.adapter.ts
+++ src/infra/criptography/bcrypt.adapter.ts
@@ -1,14 +1,19 @@
 import bcrypt from 'bcrypt'
 import { Hasher } from "../../data/protocols/hasher.interface"
+import { HashComparer } from './../../data/protocols/hash-comparer.interface'
 
-export class BCryptAdapter implements Hasher {
+export class BCryptAdapter implements Hasher, HashComparer {
   private readonly salt: number
 
   constructor (salt: number) {
     this.salt = salt
   }
-
+  
   async hash (value: string): Promise<string> {
     return await bcrypt.hash(value, this.salt)
+  }
+
+  async compare (value: string, hash: string): Promise<boolean> {
+    return await bcrypt.compare(value, hash)
   }
 }

a1271faf1133ab222952bab09864e7bb82af447e
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2022-12-20 23:51:05 -0300

    refactor: rename MONGO_URI

--- src/main/config/env.ts
+++ src/main/config/env.ts
@@ -2,6 +2,6 @@ export default {
   port: process.env.PORT ?? 5050,
   jwtSecret: process.env.JWT_SECRET ?? 'cle@nNodeApi1',
   salt: 12,
-  mongoUrl: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/clean-node-api',
-  mongoUrlTest: process.env.MONGODB_URI ?? 'mongodb://localhost:27017/clean-node-api-test'
+  mongoUrl: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api',
+  mongoUrlTest: process.env.MONGO_URL ?? 'mongodb://localhost:27017/clean-node-api-test'
 }

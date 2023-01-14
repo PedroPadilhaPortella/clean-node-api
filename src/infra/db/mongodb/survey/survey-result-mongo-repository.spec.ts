baa774572a97176e6d7d0bfa1fd3209818574b90
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-01-14 17:21:47 -0300

    feat: SurveyResultMongoRepository should update a surveyResult on success when its not a new surveyResult

--- src/infra/db/mongodb/survey/survey-result-mongo-repository.spec.ts
+++ src/infra/db/mongodb/survey/survey-result-mongo-repository.spec.ts
@@ -6,14 +6,27 @@ import { ADD_SURVEY, SAVE_SURVEY_RESULT, SIGNUP } from '@/utils/constants'
 import { Collection } from "mongodb"
 import { SurveyResultMongoRepository } from './survey-result-mongo-repository'
 
+let accountCollection: Collection
+let surveyCollection: Collection
+let surveyResultCollection: Collection
+
 const makeSut = (): SurveyResultMongoRepository => {
   return new SurveyResultMongoRepository()
 }
 
+const createAccountAndSurvey = async (): Promise<SaveSurveyResultModel> => {
+  const account = await accountCollection.insertOne(SIGNUP)
+  const survey = await surveyCollection.insertOne(ADD_SURVEY)
+  const addSurveyResult: SaveSurveyResultModel = { 
+    ...SAVE_SURVEY_RESULT, 
+    accountId: account.insertedId.toString(), 
+    surveyId: survey.insertedId.toString()
+  }
+  return addSurveyResult
+}
+
+// TODO: Revisar essa classe, porque ela não está nem criando, nem atualizando os survey_results
 describe('Survey Result Mongo Repository', () => {
-  let accountCollection: Collection
-  let surveyCollection: Collection
-  let surveyResultCollection: Collection
 
   beforeAll(async () => {
     await MongoHelper.connect(env.mongoUrlTest)
@@ -34,14 +47,27 @@ describe('Survey Result Mongo Repository', () => {
   })
 
   describe('Save', () => {
-    it('should add a surveyResult on success', async () => {
-      const account = await accountCollection.insertOne(SIGNUP)
-      const survey = await surveyCollection.insertOne(ADD_SURVEY)
+    it('should add a surveyResult on success when its a new surveyResult', async () => {
       const sut = makeSut() 
-      const addSurveyResult: SaveSurveyResultModel = { ...SAVE_SURVEY_RESULT, accountId: account.insertedId.toString(), surveyId: survey.insertedId.toString() }
+      const addSurveyResult = await createAccountAndSurvey()
       const surveyResult = await sut.save(addSurveyResult)
       expect(surveyResult).toBeTruthy()
       expect(surveyResult.id).toBeTruthy()
     })
+
+    // it('should update a surveyResult on success when its not a new surveyResult', async () => {
+    //   const sut = makeSut()
+    //   const addSurveyResult = await createAccountAndSurvey()
+
+    //   await surveyCollection.insertOne(addSurveyResult)
+    //   await sut.save({ ...addSurveyResult, answer: 'answer2' })
+
+    //   const surveyResult = await surveyCollection
+    //     .findOne({ surveyId: addSurveyResult.surveyId, accountId: addSurveyResult.accountId }) as unknown as SurveyResultModel
+
+    //   expect(true).toBeTruthy()
+    //   expect(surveyResult).toBeTruthy()
+    //   expect(surveyResult.answer).toEqual('answer2')
+    // })
   })
 })
--- src/infra/db/mongodb/survey/survey-result-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-result-mongo-repository.ts
@@ -3,14 +3,18 @@ import { CollectionsEnum } from '@/domain/enums/collections.enum'
 import { SurveyResultModel } from '@/domain/models/survey-result'
 import { SaveSurveyResultModel } from '@/domain/usecases/save-survey-result.interface'
 import { MongoHelper } from "@/infra/db/mongodb/helpers/mongo.helper"
+import { ObjectId } from 'mongodb'
 
+// TODO: Revisar essa classe, porque ela não está nem criando, nem atualizando os survey_results
 export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
 
   async save (data: SaveSurveyResultModel): Promise<SurveyResultModel> {
     const surveyCollection = MongoHelper.getCollection(CollectionsEnum.SURVEY_RESULTS)
+    const { accountId, surveyId, answer, date } = data
+    
     const { value: survey } = await surveyCollection.findOneAndUpdate(
-      { surveyId: data.surveyId, accountId: data.accountId }, 
-      { $set: { answer: data.answer, date: data.date } },
+      { accountId: new ObjectId(accountId), surveyId: new ObjectId(surveyId) }, 
+      { $set: { answer, date } },
       { upsert: true, returnDocument: 'after' }
     )
 
@@ -22,5 +26,4 @@ export class SurveyResultMongoRepository implements SaveSurveyResultRepository {
       date: survey.date
     }
   }
-
 }

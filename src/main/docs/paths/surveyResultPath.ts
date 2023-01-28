fc5e2a2f4319a44f10cdf59131c9342f288416e2
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-01-28 14:48:17 -0300

    docs: add LoadSurveyResult api swagger docs

--- src/main/docs/paths/surveyResultPath.ts
+++ src/main/docs/paths/surveyResultPath.ts
@@ -45,5 +45,43 @@ export const surveyResultPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    tags: ['Enquete'],
+    summary: 'Buscar as respostas de uma Enquete',
+    security: [
+      { ApiKeyAuth: [] }
+    ],
+    parameters: [
+      {
+        in: 'path',
+        name: 'surveyId',
+        required: true,
+        schema: {
+          type: 'string'
+        }
+      }
+    ],
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/SurveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }

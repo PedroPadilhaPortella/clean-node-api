3fb370805fde2e12c8fe8882b048dd80c8302462
Pedro Padilha Portella <pedro.kadjin.sg@gmail.com>
2023-01-28 23:50:28 -0300

    chore: release 2.4.2

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -7,7 +7,7 @@ export default {
   info: {
     title: 'Clean Node API',
     description: 'API de enquetes entre programadores',
-    version: '2.4.1'
+    version: '2.4.2'
   },
   contact: {
     name: 'Pedro Padilha Portella',
